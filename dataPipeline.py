96651a84de7b2c39289fc7d8b1f56440adb9c18f
Jacob <sjacob0412@gmail.com>
2020-05-19 18:52:04 -0400

    adds attack localization to model

---
+++ dataPipeline.py
@@ -0,0 +1,68 @@
+from scipy.io import arff
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+class DataPipeline():
+
+    def loadData(self, path = 'input/', num_sets = 15):
+        '''
+        This function loads the data from the path variable provided and returns the data as a pandas dataframe
+        '''
+        print('Loading dataset #1')
+        data = pd.DataFrame(arff.loadarff('{}power_multiclass/data1.arff'.format(path))[0])
+
+        for i in range(2,num_sets+1):
+            print('Loading dataset #{}'.format(i))
+            dataTemp = pd.DataFrame(arff.loadarff('{}power_multiclass/data{}.arff'.format(path,i))[0])
+            data = pd.concat([data,dataTemp],axis=0)
+
+        print("Finished Loading. Final Size = {}".format(data.shape))
+
+        return data
+
+    def getFeatureLabels(self, df):
+        '''
+        This function takes a dataframe and returns the labels of used features
+        '''
+        df = df.astype(np.float64)
+        label = df['marker'].astype(int)
+        df = df.drop(['marker'], axis=1)
+
+        df = df.drop(['snort_log1','snort_log2','snort_log3','snort_log4',
+                    'control_panel_log1','control_panel_log2','control_panel_log3','control_panel_log4',
+                    'relay1_log','relay2_log','relay3_log','relay4_log'], axis=1)
+
+        df = df.reset_index()
+
+        return df.columns
+
+
+    def dataProc(self, df):
+        '''
+        This function takes a dataframe and splits it into data and labels, proccesses them into numpy arrays and
+        splits them into training, validation, and testing data and labels.
+        '''
+        df = df.astype(np.float64)
+        label = df['marker'].astype(int)
+        df = df.drop(['marker'], axis=1)
+
+        df = df.drop(['snort_log1','snort_log2','snort_log3','snort_log4',
+                  'control_panel_log1','control_panel_log2','control_panel_log3','control_panel_log4',
+                  'relay1_log','relay2_log','relay3_log','relay4_log'], axis=1)
+
+        df = df.reset_index()
+
+        df = df.replace(-np.inf, 0)
+        df = df.replace(np.inf, 0)
+
+
+        # Converting to arrays
+        X = np.asarray(df)
+        y =  np.asarray(label)
+
+        # Scaling data
+        scalar = preprocessing.MinMaxScaler()
+        X = scalar.fit_transform(X)
+
+        return X,y
--- erlc.py
+++ erlc.py
@@ -10,11 +10,14 @@ import numpy as np
 from tensorflow.keras import backend as K
 
 
-'''
-Ensemble Representation Learning Classifier (ERLC)
-'''
+
 
 class ERLC (BaseEstimator):
+
+    '''
+    Ensemble Representation Learning Classifier (ERLC)
+    '''
+
 
     def __init__(self, verbose = True, sae_hidden_nodes = 400, innerNN_layers = 3, innerNN_nodes = 512, outerNN_layers = 2, outerNN_nodes = 256 ,pca_components = 14):
         self.verbose = verbose
@@ -36,6 +39,12 @@ class ERLC (BaseEstimator):
         self.outer_dnn = Sequential()
         self.pca = PCA(n_components = pca_components)
 
+        # Private class variables
+        self.normal_means = []
+        self.normal_std = []
+        self.isTrained = False
+
+
 
     def get_params(self, deep=True):
         return {"sae_hidden_nodes": self.sae_hidden_nodes,
@@ -50,7 +59,23 @@ class ERLC (BaseEstimator):
             setattr(self, parameter, value)
         return self
 
+
+
+
     def fit(self, X_train, y_train,  num_classes = 42, sae_epochs = 500, innerNN_epochs = 500, outerNN_epochs = 500):
+        '''
+        This function fits/trains the model to the inputted data.
+
+        inputs
+        --------
+        X_train: The training data
+        y_train: corresponding labels
+        num_classes: number of labels in the data
+        sae_epochs: epochs of training for the Stacked Autoencoder (SAE)
+        innerNN_epochs: epochs of training for the inner neural network
+        outerNN_epochs: epochs of training for the outer neural network
+        '''
+
         if (self.verbose):
             print("Building ERLC model")
 
@@ -143,9 +168,26 @@ class ERLC (BaseEstimator):
                                             epochs = outerNN_epochs)
 
         if (self.verbose):
+            print("Finding attack localization metrics")
+        getLocalizationStats(X_train, y_train)
+
+        if (self.verbose):
             print("Training complete")
 
+        self.isTrained = True
+
     def predict (self, X_test):
+        '''
+        This function predicts the output of the input test data.
+        This function must be called after fit has been called.
+        inputs
+        -------
+        X_test: testing data
+
+        outputs
+        -------
+        y_pred: the predicted labels of the test data
+        '''
         # Get new representation of test data
         X_test_new = self.sae.predict(X_test)
 
@@ -184,12 +226,37 @@ class ERLC (BaseEstimator):
 
         return y_pred
 
+
+    def localize (self, X_sample, y_sample):
+        '''
+        This function localizes the attack by returning the difference of each feature from that of normal samples
+        '''
+
+        if (X_sample.ndim > 1):
+            raise ValueError('Sample array must be 1 dimensional')
+        if (y_sample.ndim > 1):
+            raise ValueError('Sample label must be 1 dimensional')
+
+        p_diff_final = self.getAveragePDforEachAttack(X_sample, y_sample)
+        p_diff_final = p_diff_final[::-1].sort()
+
+        return p_diff_final
+
+
+
     def buildSAE(self, X_train, num_nodes = 400, epochs= 100):
         '''
         This function builds the Stacked AutoEncoder (SAE) and trains it to gain a new representation.
-        INPUTS:
-        - data: matrix of the data (meter measurements of a smart grid)
-        - num_nodes: the number of nodes in the hidden layer (default = 400)
+
+        inputs
+        -------
+        X_train: matrix of the data
+        num_nodes: the number of nodes in the hidden layer
+        epochs: number of epochs to train the SAE model
+
+        outputs
+        --------
+        model: the trained SAE model
         '''
 
         input_X= Input(shape=(X_train.shape[1],))
@@ -218,40 +285,166 @@ class ERLC (BaseEstimator):
 
 
 
-    def buildNN(self, X_train, y_train, num_layers = 2, num_nodes = 128, activation = 'relu', num_classes = 42, do = 0, regularizer = False, epochs = 500, val_split = 0):
+    def buildNN(self, X_train, y_train, num_classes = 42, num_layers = 2, num_nodes = 128, activation = 'relu', do = 0, regularizer = False, epochs = 500, val_split = 0):
         '''
         This function builds the inner Deep Neural Network (DNN) and trains it to gain a new representation.
-        INPUTS:
-        - X_train: matrix of the data (meter measurements of a smart grid)
-        - y_train: array of the labels for the corresponding X_train samples
-        - the autoencoder model built with buildSAE function
-        - num_classes: the number of classes
+
+        inputs
+        --------
+        X_train: matrix of the data (meter measurements of a smart grid)
+        y_train: array of the labels for the corresponding X_train samples
+        num_classes: the number of classes
+        num_layers: the number of hidden layers in the neural network
+        num_nodes: the number of nodes in each hidden layer
+        activation: the activation function in each layer (except the final layer)
+        do: percent of dropout in between the hidden layers. This should be a value between 0 and 1. If 0, dropout will not be used
+        regularizer: whether or not to use l2 regularization in hidden layers
+        epochs: number of epochs to train the network
+        val_split: percentage of data to use for validation as the network is being trained. This is a value between 0 and 1.
+
+        outputs
+        --------
+        nn_model: The trained neural network
         '''
+
         # Building the Neural Network
         y_train2 = to_categorical(y_train,num_classes= num_classes)
-        outer_nn_model = Sequential()
+        nn_model = Sequential()
 
         for i in range(num_layers):
             if ( (i> 0) & (i < num_layers-1) & (do > 0.0)):
-                outer_nn_model.add(Dropout(do))
+                nn_model.add(Dropout(do))
 
             if (regularizer == True):
-                outer_nn_model.add(Dense(num_nodes, activation=activation, kernel_regularizer= tf.keras.regularizers.l2(0.0001)))
+                nn_model.add(Dense(num_nodes, activation=activation, kernel_regularizer= tf.keras.regularizers.l2(0.0001)))
             else:
-                outer_nn_model.add(Dense(num_nodes, activation=activation))
+                nn_model.add(Dense(num_nodes, activation=activation))
 
-        outer_nn_model.add(Dense(num_classes, activation='softmax'))
-        outer_nn_model.compile(optimizer='adam', loss= 'categorical_crossentropy', metrics=['acc',self.f1_m])
+        nn_model.add(Dense(num_classes, activation='softmax'))
+        nn_model.compile(optimizer='adam', loss= 'categorical_crossentropy', metrics=['acc',self.f1_m])
 
         # Early Stop Callback
         earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=1e-6, mode = 'min',patience=10)
 
         if (val_split>0):
-            outer_nn_model.fit(X_train, y_train2, epochs = epochs, batch_size=128, validation_split=val_split, callbacks = [earlystop_callback])
+            nn_model.fit(X_train, y_train2, epochs = epochs, batch_size=128, validation_split=val_split, callbacks = [earlystop_callback])
         else:
-            outer_nn_model.fit(X_train, y_train2, epochs = epochs, batch_size=128, callbacks = [earlystop_callback])
-        return outer_nn_model
+            nn_model.fit(X_train, y_train2, epochs = epochs, batch_size=128, callbacks = [earlystop_callback])
 
+        return nn_model
+
+
+    def getAveragePDforEachAttack(self, X_test, y_test):
+        '''
+        This function gets the average percent difference for each attack type
+
+        inputs
+        --------
+        X_test: the testing data
+        y_test: the corresponding labels of testing data
+
+
+        outputs
+        --------
+        p_diff_avg_final: A matrix corresponding to the percent difference of each feature. The size of this matrix is the same as
+        that of X_test
+        '''
+
+        p_diff_avg_final = []
+
+        for i in np.unique(y_test):
+            print("Iteration i = {}".format(i))
+            p_diff_temp = self.getPD(X_test[y_test==i], self.normal_means, self.normal_std)
+            p_diff_temp = np.asarray(p_diff_temp)
+
+            print("p_diff_temp shape = {}".format(p_diff_temp.shape))
+
+            p_diff_avg = []
+            for j in range(p_diff_temp.shape[0]):
+                avg_temp = np.mean(p_diff_temp[j])
+                p_diff_avg.append(avg_temp)
+
+            p_diff_avg = np.asarray(p_diff_avg)
+            print("p_diff_avg shape = {}".format(p_diff_avg.shape))
+            p_diff_avg_final.append(p_diff_avg)
+
+
+        p_diff_avg_final = np.asarray(p_diff_avg_final)
+
+        return p_diff_avg_final
+
+    def getLocalizationStats (self, X, y, normal_label = 41):
+        '''
+        This function gets the localization statistics, which are the mean and standard deviation of each measurement
+        under normal observations. It returns means and stds to be used in percent difference calculation for attack
+        localization.
+
+        inputs
+        --------
+        X: the training data
+        y: the training labels
+        normal_label: the value of the label that refers to normal samples
+        '''
+
+        means = []
+        stds = []
+
+        for i in range(X[y==normal_label].shape[1]):
+            mean_temp = np.mean(X[:,i])
+            std_temp = np.std(X[:,i])
+
+            # Replacing zeros with small numbers
+            if (mean_temp == 0):
+                mean_temp = 0.000001
+            if (std_temp == 0):
+                std_temp = 0.000001
+
+            # Adding them to a list
+            means.append(mean_temp)
+            stds.append(std_temp)
+
+        means = np.asarray(means)
+        stds = np.asarray(stds)
+
+        self.normal_means = means
+        self.normal_std = stds
+
+
+    def getPD(self, sample_np, normal_means, normal_stds):
+        '''
+        This function takes in a sample as a numpy array and calculates the percent difference from the mean
+        of every measurement from the normal data.
+
+        inputs
+        -------
+        sample_np: Sample in numpy format
+        normal_means: means of the normal data (from getLocalizationStats)
+        normal_stds: standard deviation of the normal data (from getLocalizationStats)
+
+        outputs
+        -------
+        p_diff: percent difference of the sample
+        '''
+
+        p_diff = []
+
+        for i in range(sample_np.shape[1]):
+            p_diff_temp = []
+            for j in range(sample_np.shape[0]):
+                comparison_value = abs(normal_means[i])
+                # comparison_value = abs(normal_means[j])
+                temp = sample_np[j][i]
+                percent_diff = abs((temp - comparison_value))
+                percent_diff = abs((percent_diff - normal_stds[i])/normal_stds[i])
+                p_diff_temp.append(percent_diff)
+
+            p_diff.append(p_diff_temp)
+        p_diff = np.asarray(p_diff)
+
+        return p_diff
+
+
+    ## METRICS
     def recall_m(self, y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
